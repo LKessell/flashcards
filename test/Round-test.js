1e40162b8fca0fbb64fd96b6eaa25c4a0be984f9
Lauren Kessell <lkessell1@gmail.com>
2021-05-11 19:18:49 -0400

    Add test to return current card

--- test/Round-test.js
+++ test/Round-test.js
@@ -61,4 +61,14 @@ describe('Round', function() {
     const round = new Round(deck);
     expect(round.incorrectGuesses).to.deep.equal([]);
   });
+
+  it('should return the current card being played', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    expect(round.returnCurrentCard()).to.deep.equal({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
+  });
 });
