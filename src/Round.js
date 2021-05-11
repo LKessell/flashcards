b5a511fade038f595d7499c3a1e41f4654392826
Lauren Kessell <lkessell1@gmail.com>
2021-05-11 19:53:22 -0400

    Increment turns after guessing

--- src/Round.js
+++ src/Round.js
@@ -1,3 +1,5 @@
+const Turn = require('../src/Turn');
+
 class Round {
   constructor(deck) {
     this.deck = deck.cards;
@@ -6,6 +8,11 @@ class Round {
   }
   returnCurrentCard() {
     return this.deck[this.turns];
+  }
+  takeTurn(guess) {
+    const currentCard = this.returnCurrentCard();
+    const turn = new Turn(guess, currentCard);
+    this.turns++;
   }
 }
 
