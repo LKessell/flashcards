d044d02145bad6e0101f797e8cfd3453a774a20b
Lauren Kessell <lkessell1@gmail.com>
2021-05-11 18:21:16 -0400

    Add test for Deck argument

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,7 +4,7 @@ const expect = chai.expect;
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
-const Turn = require('..src/Turn');
+const Turn = require('../src/Turn');
 
 describe('Round', function() {
 
@@ -17,5 +17,14 @@ describe('Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceof(Round);
   });
-  
+
+  it('should take a Deck as an argument', function() {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    const card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    expect(round.deck).to.be.an.instanceof(Deck);
+  });
 });
