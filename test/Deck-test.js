2b170f9d9b3d369306fd0d40861fe583c6ad90d2
Lauren Kessell <lkessell1@gmail.com>
2021-05-11 15:25:36 -0400

    Add tests for instantiation

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,4 +6,13 @@ const Card = require('../src/Card');
 
 describe('Deck', function() {
 
+  it('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
 });
