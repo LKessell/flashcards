a86720f6eea8532b68c2eb9bb2f9e087c82d2d52
Lauren Kessell <lkessell1@gmail.com>
2021-05-11 16:12:36 -0400

    Add tests for instantiation

--- test/Round-test.js
+++ test/Round-test.js
@@ -8,4 +8,14 @@ const Turn = require('..src/Turn');
 
 describe('Round', function() {
 
-})
+  it('should be a function', function() {
+    const round = new Round();
+    expect(Round).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+  
+});
