348d41ca6f2c0538f77b354d3d47c500c64ae8c9
Lauren Kessell <lkessell1@gmail.com>
2021-05-12 17:29:59 -0400

    Set up Game test file

---
+++ test/Game-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Game', function() {
+  let card1;
+  let card2;
+  let card3;
+  let cards;
+  let deck;
+  let round;
+  beforeEach('set up', function() {
+    card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
+    card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
+    cards = [card1, card2, card3];
+    deck = new Deck(cards);
+    round = new Round(deck);
+  });
+})
