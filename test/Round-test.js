407de18e369133da9c42078fd6d75aaba458a536
Lauren Kessell <lkessell1@gmail.com>
2021-05-13 17:02:19 -0400

    Improve beforeEach and spacing

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,21 +4,20 @@ const expect = chai.expect;
 const Round = require('../src/Round');
 const Deck = require('../src/Deck');
 const Card = require('../src/Card');
-const Turn = require('../src/Turn');
 
 describe('Round', function() {
+
   let card1;
   let card2;
   let card3;
-  let cards;
   let deck;
   let round;
+
   beforeEach('set up', function() {
     card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array');
     card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method');
-    cards = [card1, card2, card3];
-    deck = new Deck(cards);
+    deck = new Deck([card1, card2, card3]);
     round = new Round(deck);
   });
 
@@ -52,13 +51,17 @@ describe('Round', function() {
 
   it('should be able to take a turn by guessing', function() {
     expect(round.turns).to.equal(0);
+
     round.takeTurn('object');
+
     expect(round.turns).to.equal(1);
   });
 
   it('should move on to the next card after a guess is made', function() {
     expect(round.returnCurrentCard()).to.deep.equal({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
+
     round.takeTurn('array');
+
     expect(round.returnCurrentCard()).to.deep.equal({id: 2, question: 'What is a comma-separated list of related values?', answers: ['array', 'object', 'function'], correctAnswer: 'array'});
   });
 
@@ -72,17 +75,24 @@ describe('Round', function() {
 
   it('should store incorrect guesses', function() {
     expect(round.incorrectGuesses).to.deep.equal([]);
+
     round.takeTurn('array');
+
     expect(round.incorrectGuesses).to.deep.equal([1]);
+
     round.takeTurn('object');
+
     expect(round.incorrectGuesses).to.deep.equal([1, 2]);
   });
 
   it('should be able to calculate the percentage of questions answered correctly', function() {
     round.takeTurn('object');
     round.takeTurn('object');
+
     expect(round.calculatePercentCorrect()).to.equal(50);
+
     round.takeTurn('mutator method');
+
     expect(round.calculatePercentCorrect()).to.equal(66);
   });
 
@@ -90,6 +100,7 @@ describe('Round', function() {
     round.takeTurn('object');
     round.takeTurn('array');
     round.takeTurn('mutator method');
+    
     expect(round.endRound()).to.equal('** Round over! ** You answered 100% of the questions correctly!');
   });
 });
