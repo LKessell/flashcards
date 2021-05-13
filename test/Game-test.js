19f39197501d8307cb4adb5b9ebba5df53161467
Lauren Kessell <lkessell1@gmail.com>
2021-05-12 20:19:13 -0400

    Refactor and clean up code

--- test/Game-test.js
+++ test/Game-test.js
@@ -3,30 +3,27 @@ const expect = chai.expect;
 
 const Game = require('../src/Game');
 const Round = require('../src/Round');
-const Deck = require('../src/Deck');
-const Card = require('../src/Card');
-const Turn = require('../src/Turn');
 
 describe('Game', function() {
+  let game;
+  beforeEach('set up', function() {
+    game = new Game();
+  });
 
   it('should be a function', function() {
-    const game = new Game();
     expect(Game).to.be.a('function');
   });
 
-  it('should be an instance of Round', function() {
-    const game = new Game();
+  it('should be an instance of Game', function() {
     expect(game).to.be.an.instanceof(Game);
   });
 
   it('should start with no currentRound', function() {
-    const game = new Game();
     expect(game.currentRound).to.deep.equal(null);
   });
 
   it('should be able to start a new round', function() {
-    const game = new Game();
     game.start();
     expect(game.currentRound).to.be.an.instanceof(Round);
   });
-})
+});
