59b4d8c503ee619cc9be352933cc733bb65ec8c5
Lauren Kessell <lkessell1@gmail.com>
2021-05-12 20:22:29 -0400

    Build start method

--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,23 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Round = require('./Round');
+const Deck = require('./Deck');
+const Card = require('./Card');
+
 class Game {
   constructor() {
     this.currentRound = null;
+  }
+
+  start() {
+    const cards = prototypeQuestions.map(cardData => {
+      return new Card(cardData.id, cardData.question, cardData.answers, cardData.correctAnswer);
+    });
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
   }
 
   printMessage(deck, round) {
